9350c727e8daf84a48204869e29965d65de39633
ZhenHangZhang <zhanghangzhen@Z.local>
2016-06-21 10:32:47 +0800

    测试1
    都

---
+++ ZHZ_test/BtnViewController.swift
@@ -0,0 +1,28 @@
+
+//
+//  BtnViewController.swift
+//  ZHZ_test
+//
+//  Created by zhanghangzhen on 16/6/20.
+//  Copyright © 2016年 zhanghangzhen. All rights reserved.
+//
+
+import UIKit
+
+class BtnViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+self.view.backgroundColor = UIColor.redColor()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
--- ZHZ_test/ViewController.swift
+++ ZHZ_test/ViewController.swift
@@ -12,9 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        self.title = "按钮"
+        for i in 1..<5 {
+            
+            let btn = UIButton(type: .Custom)
+            view.addSubview(btn)
+            btn.tag = i
+            btn.frame = CGRectMake(0, CGFloat(60*i), view.frame.size.width, 50)
+            btn.backgroundColor = UIColor.redColor()
+            btn.setTitle(("按钮"+"\(i)"), forState: .Normal)
+            btn.addTarget(self, action: #selector(ViewController.zaijian(_:)), forControlEvents: .TouchUpInside)
+        }
     }
-
+    func zaijian(sender:UIButton){
+        print("nihao" + "\(sender.tag)")
+        switch sender.tag {
+        case 1:
+            let btnVC = BtnViewController();
+            self.navigationController!.pushViewController(btnVC, animated: true)
+            print("nihao")
+        case 2:
+            let textVc = TextViewController()
+            self.navigationController?.pushViewController(textVc, animated: true)
+            print("nihao" + "\(sender.tag)")
+        default: break
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
