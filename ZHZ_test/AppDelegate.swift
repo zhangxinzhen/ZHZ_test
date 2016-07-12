5c57703ad8ca3246c16009d0c967515120b97cb5
ZhenHangZhang <zhanghangzhen@Z.local>
2016-07-12 16:20:34 +0800

    特

--- ZHZ_test/AppDelegate.swift
+++ ZHZ_test/AppDelegate.swift
@@ -12,13 +12,32 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    let dictBundle = NSBundle.mainBundle().infoDictionary
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        let restates = customApplication(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        return restates
     }
 
+   private func customApplication(application:UIApplication,didFinishLaunchingWithOptions launchOptions:[NSObject: AnyObject]?)  -> Bool {
+    
+    
+    
+    
+    let urlStr = "http://b2c.ezparking.com.cn/rtpi-service/misc/token.do?deviceId=\(OpenUDID.value)&app=\(dictBundle!["CFBundleIdentifier"])&version=\(dictBundle!["CFBundleVersion"])"
+    
+    print("\(urlStr)")
+    
+    
+    
+    
+    
+    
+    
+    
+        return true
+    }
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
--- ZHZ_test/ViewController.swift
+++ ZHZ_test/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  ZHZ_test
-//
-//  Created by zhanghangzhen on 16/6/20.
-//  Copyright © 2016年 zhanghangzhen. All rights reserved.
-//
 
 import UIKit
 
